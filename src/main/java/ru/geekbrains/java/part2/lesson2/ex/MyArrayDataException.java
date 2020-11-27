b17571b35dd0210e094ff34c10ef64a4466a9139
ssomkov <ssomkov@gmail.com>
2020-11-27 17:27:04 +0400

    completed lesson2 from java2

---
+++ src/main/java/ru/geekbrains/java/part2/lesson2/ex/MyArrayDataException.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.java.part2.lesson2.ex;
+
+public class MyArrayDataException extends Exception {
+
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
